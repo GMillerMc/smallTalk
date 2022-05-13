76d6042dd2e3cd0e2c0d83aba4ceec97ae49cb82
dartbill <billieldartnell@hotmail.com>
2022-05-13 15:09:53 +0100

    first commit-server running

---
+++ app.js
@@ -0,0 +1,22 @@
+//allows express to be used
+const express = require('express');
+
+// allows cors to be used
+const cors = require('cors');
+
+//create a new express application
+const app = express();
+
+
+// allows for json to be read
+app.use(express.json()) 
+app.use(cors());
+
+
+//sets homepage to html
+app.get('/', (req, res) => {
+    res.send("hello world")
+});
+
+
+module.exports = app;
